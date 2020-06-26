f8b5b31a31385ef7ddafa3e849bab1089573ed52
ivanpriyambudi <gendoh75@gmail.com>
2020-06-26 07:01:48 +0700

    Add files via upload

---
+++ tabel-artikel.js
@@ -0,0 +1,25 @@
+{
+  "data": [
+    {
+      "id": "1",
+      "judul": "Judul 1",
+      "tag": "tutorial",
+      "status": "Pending",
+      "tanggal": "23 Juni 2020"
+    },
+    {
+      "id": "2",
+      "judul": "Judul 2",
+      "tag": "tutorial",
+      "status": "Terbit",
+      "tanggal": "22 Juni 2020"
+    },
+    {
+      "id": "3",
+      "judul": "Judul 3",
+      "tag": "tutorial",
+      "status": "Terbit",
+      "tanggal": "21 Juni 2020"
+    }
+  ]
+}
